7c657407e19b8effd41f80256a43d52bbc870787
Alexander Litos <alexander.litos@gmail.com>
2016-09-13 11:43:58 +0200

    En första commit

---
+++ Radera/MyScripts/site.js
@@ -0,0 +1,21 @@
+﻿// site.js
+(function () {
+    
+    //Menu toggle 2
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    var $icon = $("#menuToggle i.glyphicon");
+
+    $("#menuToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("glyphicon-chevron-left");
+            $icon.addClass("glyphicon-chevron-right");
+        }
+        else {
+            $icon.addClass("glyphicon-chevron-left");
+            $icon.removeClass("glyphicon-chevron-right");
+        }
+    });
+
+})();
