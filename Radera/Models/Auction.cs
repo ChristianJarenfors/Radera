73ace5c07ccf1bb56a7efa74e851455de85a113c
linus erikson <linuserikson@gmail.com>
2016-09-13 13:35:29 +0200

    Added EF and database model

---
+++ Radera/Models/Auction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Radera.Models
+{
+    public class Auction 
+    {
+        public int AuctionID { get; set; }
+        public string Picture { get; set; }
+        public IList<string> SearchTags { get; set; }
+        public int PriceBuyout { get; set; }
+
+        public virtual User AuctionOwner { get; set; }
+        public virtual IList<Bid> Bids { get; set; }
+
+        public virtual IList<Comment> Comments { get; set; }
+    }
+}
---
+++ Radera/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Radera.Models
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public DateTime Date { get; set; }
+        public string CommentMessage { get; set; }
+
+        public virtual User CommentOwner { get; set; }
+        public virtual Auction CommentAuction { get; set; }
+
+    }
+}
